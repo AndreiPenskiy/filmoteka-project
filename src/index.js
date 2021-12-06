eb583e63cfdc7ea880eb9e953295c7ba4bc7d7ff
Sunwart <li.sa.vectorart@gmail.com>
2021-12-06 21:18:35 +0200

    API-service and search function added
    додала багато чого в index.js для прикладу. Потім можна буде почистити...

--- src/index.js
+++ src/index.js
@@ -5,3 +5,24 @@ import './sass/main.scss';
 //import Pagination from 'tui-pagination';
 
 import './js/pagination.js';
+import filmsAPIService from './js/API-service';
+import { searchFilms } from './js/search';
+
+const trendingFilms = new filmsAPIService();
+trendingFilms.page = 3; //приклад використання для пагінації
+trendingFilms.language = 'en'; //приклад використання для локалізації
+
+//ДАНІ З БЕКЕНДУ ДЛЯ ГОЛОВНОЇ СТОРІНКИ
+trendingFilms.getTrendingFilms().then(res => console.log('популярні фільми: ', res.data));
+// підказки тут https://docs.google.com/document/d/1Hrx6Rgc6hSu4L69pmSNMm8UyLrBKyviCQcfNAZEx5Q4/edit?usp=sharing
+
+//приклад використання - витягнути дані одного фільму по його id
+trendingFilms.getSingleFilmByID(497698).then(res => console.log('окремий фільм:', res.data));
+// підказки тут https://docs.google.com/document/d/1Hrx6Rgc6hSu4L69pmSNMm8UyLrBKyviCQcfNAZEx5Q4/edit?usp=sharing
+
+//приклад використання - ВСІ ЖАНРИ
+trendingFilms.getGenres().then(res => console.log('всі жанри', res.data));
+
+//РЕАЛІЗАЦІЯ ПОШУКУ (без рендерінгу розмітки)
+const searchInput = document.querySelector('.search-form');
+searchInput.addEventListener('submit', searchFilms);
---
+++ src/js/api-service.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+export default class filmsAPIService {
+  constructor() {
+    this.currentPage = 1; // можна використовувати для пагінації
+    this.lang = 'uk';
+  }
+
+  getTrendingFilms() {
+    return axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=daf1fe8995a61d2fecc007eaa464ca98&page=${this.currentPage}&language=${this.lang}`,
+    );
+  }
+
+  getFilmsByQuery(query) {
+    return axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=daf1fe8995a61d2fecc007eaa464ca98&query=${query}&page=${this.currentPage}&include_adult=false&language=${this.lang}`,
+    );
+  }
+
+  getSingleFilmByID(filmID) {
+    return axios.get(
+      `https://api.themoviedb.org/3/movie/${filmID}?api_key=daf1fe8995a61d2fecc007eaa464ca98&language=${this.lang}`,
+    );
+  }
+
+  getGenres() {
+    return axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=daf1fe8995a61d2fecc007eaa464ca98&language=${this.lang}`,
+    );
+  }
+
+  get page() {
+    return this.currentPage;
+  }
+
+  set page(pageNumber) {
+    this.currentPage = pageNumber;
+  }
+
+  get language() {
+    return this.lang;
+  }
+
+  set language(userLang) {
+    this.lang = userLang;
+  }
+}
