b6748f10733505b7f0417bbecfe217ae8c29383b
HannaBatarchuk <h.batarchuk@gmail.com>
2021-12-08 21:25:44 +0200

    My lib
    - додає ховер та фокус на кнопки
    - виправлено позиціонування кнопок в хедері

--- src/js/my-library-header.js
+++ src/js/my-library-header.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('.header'); // хедер
 const form = document.querySelector('.input__wrapper'); // форма хедеру
+const searchNotification = document.querySelector('.search-notification'); // оповіщення про не вірний пошук по сайту
 const headerHome = document.querySelector('.home__btn'); // кнопка навігації Home
 const headerLibrary = document.querySelector('.my-library__btn'); // кнопка навігації My library
 const myLibraryBtnList = document.querySelector('.library-btn__list'); // кнопки хедеру My library Watched та Queue
@@ -10,6 +11,7 @@ headerLibrary.addEventListener('click', onMyLibraryBtnClick);
 function onheaderHomeBtnClick () {
     myLibraryBtnList.classList.add('is-hidden') // приховуються кнопки Watched та Queue
     form.classList.remove('is-hidden') // з'являється форма
+    searchNotification.classList.remove('is-hidden') // з'являється поле для виведення оповіщення
     headerHome.classList.add('current'); // додається помаранчеве підкреслення з кнопки Home
     headerLibrary.classList.remove('current'); // видаляється помаранчеве підкреслення кнопки My library
     header.classList.remove('header__my-library'); // видаляється фонове зображення сторінки My library
@@ -18,6 +20,7 @@ function onheaderHomeBtnClick () {
 
 function onMyLibraryBtnClick () {
     form.classList.add('is-hidden') // приховується форма
+    searchNotification.classList.add('is-hidden') // приховується поле для виведення оповіщення
     myLibraryBtnList.classList.remove('is-hidden') //з'являються кнопки Watched та Queue
     headerLibrary.classList.add('current'); // додається помаранчеве підкреслення кнопки My library
     headerHome.classList.remove('current'); // видаляється помаранчеве підкреслення з кнопки Home
