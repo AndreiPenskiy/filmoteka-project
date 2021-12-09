03cd44057d20beeafa547f75a5d5b703a03d5fa1
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-12-09 20:47:52 +0200

    changed everything

--- src/js/team.js
+++ src/js/team.js
@@ -1,16 +1,35 @@
-
- 
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+  const btns = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector(".team-modal-close-button"),
+    modal: document.querySelector(".backdrop"),
+  };
+
+  btns.openModalBtn.addEventListener("click", openModal);
+  
+  function openModal() {
+      btns.modal.classList.remove("is-hidden");
+      window.addEventListener("keydown", onPressEscape);
+      btns.closeModalBtn.addEventListener("click", closeModal);
+    btns.modal.addEventListener("click", backdropCloseModal);
+    refs.body.classList.add('scroll-hidden');
     };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+  function closeModal() {
+refs.body.classList.remove('scroll-hidden');
+    btns.closeModalBtn.removeEventListener("click", closeModal);
+    btns.modal.classList.add("is-hidden");
+    btns.modal.removeEventListener("click", closeModal);
+  window.removeEventListener('keydown', onPressEscape);
+  }
+  function onPressEscape(event) {
+    if (event.code === 'Escape') {
+      closeModal();
     }
-  })();
+  }
+  function backdropCloseModal(event) {
+    if (event.currentTarget === event.target) {
+    closeModal();
+  }
+  }
+})();
+
