333cc0b28ab9eb45441692925d4e678baadb755c
Vitalii Shulga <v.shulga019@gmail.com>
2021-12-11 19:29:45 +0200

    Updated modal of one movie
    1- Добавил темную тему на модалку
    2- Сделал чтобы по нажатию открывалась модалка с динамически подстакленной информацией о карточке. Для возможности проверки, пока руками подставил id на первые три карточки.
    3- Немного поправил стили

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -1,34 +1,45 @@
 import modalMovieTemplate from '../templates/modal-movie.hbs';
 import * as basicLightbox from 'basiclightbox';
-import '../sass/main.scss';
+import api from './api-service';
 
+const newApi = new api();
 const movieCard = document.querySelector('.container__main');
 movieCard.addEventListener('click', openMovieModal);
 
+function fetchMovieInfo(filmID) {
+  return newApi.getSingleFilmByID(filmID).then(response => {
+    // console.log(response.data);
+    return response.data;
+  });
+}
+
 function openMovieModal(e) {
-  if (e.target.closest('.card__item')?.querySelector('.picture') === undefined) {
-    return;
-  }
   e.preventDefault();
 
-  const markup = modalMovieTemplate();
-  const modal = basicLightbox.create(markup);
-  modal.show();
-
-  // Закрытие модалки по Кнопке
-  const closeBtn = document.querySelector('.modal-movie-btn-close');
-  closeBtn.addEventListener('click', closeModalBtn);
-  function closeModalBtn() {
-    modal.close();
-    window.removeEventListener('keydown', closeModalBtn);
-  }
-
-  // Закрытие модалки по Escape
-  window.addEventListener('keydown', closeModalHandler);
-  function closeModalHandler(e) {
-    if (e.code === 'Escape') {
-      modal.close();
-      window.removeEventListener('keydown', closeModalHandler);
+  fetchMovieInfo(e.target.dataset.id).then(data => {
+    if (e.target.closest('.card__item')?.querySelector('.picture') === undefined) {
+      return;
     }
-  }
+
+    const markup = modalMovieTemplate(data);
+    const modal = basicLightbox.create(markup);
+    modal.show();
+
+    // Close modal by Button
+    const closeBtn = document.querySelector('.modal-movie-close');
+    closeBtn.addEventListener('click', closeModalByBtn);
+    function closeModalByBtn() {
+      modal.close();
+      window.removeEventListener('keydown', closeModalByBtn);
+    }
+
+    // Close modal by Escape
+    window.addEventListener('keydown', closeModalByEsc);
+    function closeModalByEsc(e) {
+      if (e.code === 'Escape') {
+        modal.close();
+        window.removeEventListener('keydown', closeModalByEsc);
+      }
+    }
+  });
 }
