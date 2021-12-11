cd9e9d6de215da82b59439156ee552717a8fa92a
yuliasemenyuk <semenyukyp@gmail.com>
2021-12-11 14:37:32 +0200

    on logo click rend add

--- src/js/homepage-rendering.js
+++ src/js/homepage-rendering.js
@@ -8,20 +8,7 @@ const main = document.querySelector('.container__main');
 const IMG_URL = `https://image.tmdb.org/t/p/w500`;
 
 homepageLink.addEventListener('click', showTrendMov);
-// function onLogoClick(event) {
-//     event.preventDefault();
-
-//     console.log('ку');
-//     trendingFilms.page = 1;
-//     trendingFilms.getTrendingFilms().then(showTrendMov);
-    // .catch(err => {
-    //         
-    //         console.log('error in function render');
-    //         movieEl.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
-    //       });
-    
-// }
-   
+  
 
 function showTrendMov(event) {
     event.preventDefault();
@@ -30,8 +17,10 @@ function showTrendMov(event) {
     
     
     trendingFilms.getTrendingFilms().then(res => res.data.results.forEach(movie => {
-        const {title, poster_path, vote_average, genre_ids, release_date} = res.data.results;
-        console.log(res.data.results);
+        const {title, poster_path, vote_average, genre_ids, release_date} = movie;
+        console.log(movie);
+
+        
 
         const movieEl = document.createElement('li');
         movieEl.classList.add('card__item');
@@ -39,7 +28,7 @@ function showTrendMov(event) {
         <img src="${IMG_URL+poster_path}" alt ="${title}" class="card__poster">
         
             <h2 class="card__title">${title}</h2>
-            <p class="card__description">${genre_ids} | ${release_date}</p>
+            <p class="card__description">${genre_ids} | ${release_date.slice(0,4)}</p>
             <p class="card__rating">${vote_average}</p>
         
         </a>`
