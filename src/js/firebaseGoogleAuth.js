d9eaaa2da98ec5bf86549932dba1be2a45c37b8f
Andrei <andreipenskiy@gmail.com>
2021-12-12 19:14:52 +0200

    update

--- src/js/firebaseGoogleAuth.js
+++ src/js/firebaseGoogleAuth.js
@@ -1,6 +1,6 @@
 
 
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUNUISB4BvcdddHlHx25A-P_y0LZzRomY",
@@ -15,10 +15,42 @@ firebase.initializeApp(firebaseConfig);
 const login = document.getElementById('signin').addEventListener('click', signinUser);
 const logout = document.getElementById('signout').addEventListener('click', signoutUser)
 
-const provider = new GoogleAuthProvider();
-
+const provider = new FacebookAuthProvider();
 
 function signinUser() {
+    const facebookProvider = new firebase.auth.FacebookAuthProvider;
+    firebase.auth().signInWithPopup(facebookProvider)
+        .then(function (data){
+          console.log(data)
+          document.getElementById('signin').classList.add('signOut');
+          document.getElementById('signout').classList.add('signIn');
+          //document.getElementById('googleUser').style.display = "block";
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
+
+    function signoutUser(){
+    firebase.auth().signOut().then(() => {
+        console.log("Sign - out successful.");
+        document.getElementById('signin').classList.remove('signOut');
+      document.getElementById('signout').classList.remove('signIn');
+      //document.getElementById('googleUser').style.display = "none";
+    }).catch(error => {
+          console.log(error)
+        })
+      }
+
+
+
+
+
+
+//const provider = new GoogleAuthProvider();
+
+
+/* function signinUser() {
     const googleAuthProvider = new firebase.auth.GoogleAuthProvider;
     firebase.auth().signInWithPopup(googleAuthProvider)
         .then(function (data){
@@ -49,5 +81,5 @@ function signoutUser(){
           document.getElementById('googleUser').innerHTML = `
           <img class="user-img" src="${data.user.photoURL}">
         `
-}
+} */
       
