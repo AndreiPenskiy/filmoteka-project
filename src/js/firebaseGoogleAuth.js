8670533135cf390278ec9f8f99da729227637cd2
Andrei <andreipenskiy@gmail.com>
2021-12-13 17:58:55 +0200

    Возврат назад

--- src/js/firebaseGoogleAuth.js
+++ src/js/firebaseGoogleAuth.js
@@ -1,6 +1,6 @@
 
 
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, FacebookAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -16,43 +16,10 @@ firebase.initializeApp(firebaseConfig);
 const login = document.getElementById('signin').addEventListener('click', signinUser);
 const logout = document.getElementById('signout').addEventListener('click', signoutUser)
 
+const provider = new GoogleAuthProvider();
 
-const provider = new FacebookAuthProvider();
 
 function signinUser() {
-    const facebookProvider = new firebase.auth.FacebookAuthProvider;
-    firebase.auth().signInWithPopup(facebookProvider)
-        .then(function (data){
-          console.log(data)
-          document.getElementById('signin').classList.add('signOut');
-          document.getElementById('signout').classList.add('signIn');
-          //document.getElementById('googleUser').style.display = "block";
-        })
-        .catch(function(error){
-            console.log(error)
-        })
-    }
-
-    function signoutUser(){
-    firebase.auth().signOut().then(() => {
-        console.log("Sign - out successful.");
-        document.getElementById('signin').classList.remove('signOut');
-      document.getElementById('signout').classList.remove('signIn');
-      //document.getElementById('googleUser').style.display = "none";
-    }).catch(error => {
-          console.log(error)
-        })
-      }
-
-
-
-
-
-
-//const provider = new GoogleAuthProvider();
-
-
-/* function signinUser() {
     const googleAuthProvider = new firebase.auth.GoogleAuthProvider;
     firebase.auth().signInWithPopup(googleAuthProvider)
         .then(function (data){
@@ -83,5 +50,5 @@ function signoutUser(){
           document.getElementById('googleUser').innerHTML = `
           <img class="user-img" src="${data.user.photoURL}">
         `
-} */
-      
+}
+       
