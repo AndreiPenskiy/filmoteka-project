e5bc05c3328735bb273d498c029a5bd1df8c4452
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-12-14 21:02:12 +0200

    Исправление библиотеки

--- src/js/library-rendering.js
+++ src/js/library-rendering.js
@@ -1,32 +1,59 @@
 import filmsAPIService from './api-service';
 
 import nothingHereUrl from '../images/library/blank-cinema.jpg';  // Це посилання на картинку для порожньої бібліотеки
-import renderMovieCard from './homepage-rendering';  // Це створення розмітки картки з головної сторінки
 
 const singleFilmByID = new filmsAPIService();
 const headerLibrary = document.querySelector('.my-library__btn'); // кнопка навігації My library
+const watchedBtn = document.querySelector('.watched-btn');
+const queueBtn = document.querySelector('.queue-btn');
+
 const main = document.querySelector('.container__main'); // main
 
-const IMG_URL = `https://image.tmdb.org/t/p/w500`;
+headerLibrary.addEventListener('click', showWatched);
+watchedBtn.addEventListener('click', showWatched);
+queueBtn.addEventListener('click', showQueue);
 
 
-// потом это буду импортировать от Василия
-// const queueID = [580489];
-let queueID = [];
-let watchedID = [];
-let filmsID = [...queueID, ...watchedID];
-
-headerLibrary.addEventListener('click', showLibrary);
-
-function showLibrary() {
-    clear();             // Функция очиски сетки
-    showBlankLibrary(); // Функция для разметки если библиотека пустая
+function showWatched() {
+    clear();             
+    let watchedID = localStorage.getItem('watched');
     
-    // const movieEl = document.createElement('li');
-    // movieEl.classList.add('card__item');
+    console.log(`это я вывожу ${watchedID}`);
 
-    // renderMovieCard(movieEl, id, poster_path, title, release_date, vote_average);
-    // main.appendChild(movieEl);
+    if (watchedID) {
+        const parceID = JSON.parse(watchedID);
+          
+        for (let i = 0; i < parceID.length; i += 1) {
+            let id = parceID[i];
+            singleFilmByID.getSingleFilmByID(id)
+                .then(response => {
+                renderCardForLib(response.data, id);
+                });
+            }
+    };
+};
+
+
+function showQueue() {
+    clear();             
+    let queueID = localStorage.getItem('queue');
+    if (!queueID || queueID.length === 0) {
+        showBlankLibrary();
+        return;
+    }
+   
+    if (queueID) {
+        const parceID = JSON.parse(queueID);
+        
+        for (let i = 0; i < parceID.length; i += 1) {
+            let id = parceID[i];
+            console.log(`'это АйДи с Локал ${id}`);
+            singleFilmByID.getSingleFilmByID(id)
+                .then(response => {
+                renderCardForLib(response.data, id);
+                });
+        }
+    };
 };
 
 // Функция очиски сетки
@@ -36,14 +63,28 @@ function clear() {
 
 // Функция для разметки если библиотека пустая
 function showBlankLibrary() {
-    if (filmsID.length > 0) {
-        return;
-  }
     main.innerHTML =
     ` <a> 
-        <p class="library"> There are no films in the library yet !</p>
+        <p class="library"> There are no films yet !</p>
         <img src="${nothingHereUrl}" alt="blank cinema">
       </a>
     `;
     pagination.style.display = 'none';
 };
+
+// Функция создания разметки карточки
+function renderCardForLib(res, id) {
+  const poster_url = `src="https://www.themoviedb.org/t/p/w500/${res.poster_path}"`;
+  const genres = res.genres[0].name;
+  const movieEl = document.createElement('li');
+  movieEl.classList.add('card__item');
+  movieEl.innerHTML = `<a class="card__link" id = "${id}" href="#">
+                <img ${poster_url} alt ="${res.title}" class="card__poster">
+        
+            <h2 class="card__title">${res.title}</h2>
+            <p class="card__description">${genres} | ${res.release_date.slice(0, 4)}</p>
+            <p class="card__rating">${res.vote_average}</p>
+        
+                </a>`;
+  main.appendChild(movieEl);
+}
