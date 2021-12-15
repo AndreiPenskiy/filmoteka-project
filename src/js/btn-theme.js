b1497ef90bc3867136f0854b1e990d4c4121d578
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-12-15 21:25:59 +0200

    Исправляет цвет пагинации

--- src/js/btn-theme.js
+++ src/js/btn-theme.js
@@ -1,21 +1,15 @@
-import { changePaginationTheme } from './pagination';
 const btnThemeHeader = document.querySelector('#theme-check');
 const bodyTheme = document.querySelector('body');
-// const tuiBtn = document.querySelectorAll('.tui-page-btn');
-// const filmNameTheme = document.querySelectorAll('.card__link');
-
 const checked = document.querySelector('.lyf')
-window.addEventListener('load', saveTheme);
-
-btnThemeHeader.addEventListener('click', onTheme);
-
 let theme = localStorage.getItem("ui-theme");
+
+window.addEventListener('load', saveTheme);
+btnThemeHeader.addEventListener('click', onTheme);
 
 function saveTheme () {
     if (theme === "dark") {
         bodyTheme.classList.add('body-theme');
         checked.setAttribute('checked', true);
-        changePaginationTheme();
     };
 };
 
@@ -25,21 +19,11 @@ function onTheme() {
     if (theme === "dark") {
         bodyTheme.classList.remove('body-theme');
         localStorage.setItem("ui-theme", "light");
-        changePaginationTheme();
         return
     };
+
     bodyTheme.classList.add('body-theme');
     localStorage.setItem("ui-theme", "dark");
-    changePaginationTheme();
 };
 
-// function onTheme(e) {
-//     bodyTheme.classList.toggle('body-theme')
-//     const tuiBtnColor = tuiBtn.forEach(item  => {
-//         item.classList.toggle('color-number')
-//     })
-//     const filmNameColor = filmNameTheme.forEach(pes => {
-//         pes.classList.toggle('color-number')
-//     })
-// }
 
