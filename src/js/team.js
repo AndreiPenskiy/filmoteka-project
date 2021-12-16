c19777031096d82c8252458d257f03d7a69f5f33
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-12-16 11:32:55 +0200

    added esc

--- src/js/team.js
+++ src/js/team.js
@@ -1,15 +1,9 @@
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+// Close modal by Escape
+  window.addEventListener('keydown', closeModalByEscape);
+  function closeModalByEscape(e) {
+    if (e.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModalByEscape);
     }
-  })();
+  }
